2f3607abfcaee793ef4a0b413386d82235ee86f5
SassythSasqutch <sasssythsasqutch@gmail.com>
2021-04-15 00:26:25 +0100

    Updated descriptions for ExcludedSubWorlds list to clarify its purpose - the last commit was a lie.

--- ext/Shared/SpLevelExcludedSubWorldList.lua
+++ ext/Shared/SpLevelExcludedSubWorldList.lua
@@ -1,4 +1,6 @@
--- Add the instance GUIDS of SubWorld you don't want to load.
+-- By default, all SubWorld instances are enabled (autoLoad = true): they contain everything you need for the sub-levels SP and COOP levels are split into.
+-- This acts as a 'blacklist' for SubWorld instances. As such, the SubWorldReferenceObjectData GUIDs in this list aren't allowed to load, and all others are.
+
 -- You might also need to manually disable autoLoad for that SubWorld. See 'vu-spterrains/ext/Shared/SpLevelExcludeSubWorldExample.lua' for an example.
 
 spLevelExcludedSubWorldList = {
