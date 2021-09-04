d049770be2948bc2590efa60bc7aa229206cd728
SassythSasqutch <sasssythsasqutch@gmail.com>
2021-09-04 21:52:35 +0100

    To many changes to count, but what matters is the implementation of UI (TeamDeathMatchC0 preset only). Fuck yes.

--- ext/Shared/SpLevelDataModify.lua
+++ ext/Shared/SpLevelDataModify.lua
@@ -1,6 +1,39 @@
 require '__shared/SpLevelExcludedSubWorldList'
 
--- TODO: SP loading screens
+-- Set SP loading screens (COOP are already OK - seem to be already designed for MP) - CURRENTLY DYSFUNCTIONAL, MAY NOT BE EASILY POSSIBLE
+
+Events:Subscribe('Extension:Loaded', function()
+
+    local spUILevelDescriptionComponentGuids = {
+        '48FCF68E-9643-9EDE-C162-DBDC7C5A60DE', -- Operation Guillotine SP_Bank
+        '56C9CB41-3AA6-AB66-FCE9-4AE293ACC7F8', -- Operation Swordbreaker SP_Earthquake
+        '4BD8BF47-4ACD-4EA7-C04D-F3F17C9F3635', -- Uprising SP_Earthquake2
+        '18E70353-5B15-5E05-579D-44A3562D71B6', -- The Great Destroyer SP_Finale
+        '3B9CD74A-844C-ABCC-369B-E02F7FF9BECB', -- Going Hunting SP_Jet
+        '4D019FE2-07A8-5D69-7902-06461C370531', -- Semper Fidelis SP_New_York
+        'D954439B-DDA3-99FD-46A8-DD22D8C22B5F', -- Comrades SP_Paris
+        '8368F7BF-C156-EB46-FCCE-82CC7E92E2AD', -- Night Shift SP_Sniper
+        '6D701F2A-DCB4-3351-FEC5-6334F96688FB', -- Thunder Run SP_Tank
+        'FD7D2AC3-19DC-4B82-1598-34DD7AC5BA47', -- Fear No Evil SP_Tank_B
+        '480B7A3D-E517-62B4-75FF-A91393B8EB89', -- Rock And A Hard Place SP_Valley
+        '4166F3A8-DAF2-7338-EA99-B1E05948E8F9'  -- Kaffarov SP_Villa
+    }
+
+    for _, spUILevelDescriptionComponentGuid in pairs(spUILevelDescriptionComponentGuids) do
+
+        print('Fucking with loading screen...')
+        local thisUILevelDescriptionComponent_Uncasted = ResourceManager:SearchForInstanceByGuid(Guid(spUILevelDescriptionComponentGuid))
+
+        if thisUILevelDescriptionComponent_Uncasted ~= nil then
+            local thisUILevelDescriptionComponent = UILevelDescriptionComponent(thisUILevelDescriptionComponent_Uncasted)
+            thisUILevelDescriptionComponent:MakeWritable()
+            thisUILevelDescriptionComponent.mpLoadingAssetPath = 'UI/Assets/LoadingScreen'
+            thisUILevelDescriptionComponent.spLoadingAssetPath = ''
+        end
+
+    end
+
+end)
 
 Events:Subscribe('Partition:Loaded', function(partition) -- Iterates through every single partition so, if there is an instance in one of them we want to change, we can do so.
 
@@ -25,17 +58,12 @@ Events:Subscribe('Partition:Loaded', function(partition) -- Iterates through eve
             local thisInstance = LevelDescriptionAsset(instance)
             thisInstance:MakeWritable()
 
-            -- Enable map for MP
-            print('Setting isMultiplayer in \'' .. partition.name .. '\'...')
+            -- Enable map for MP in LevelDescriptionAsset
             thisInstance.description.isCoop = false
             thisInstance.description.isMultiplayer = true
             thisInstance.description.isMenu = false
 
-            -- Enable map for CQL
-            print('Setting LevelDescriptionInclusionCategory in \'' .. partition.name .. '\'...')
-            -- Temp
-            local catToClear -- What the fuck is this?
-            -- Temp
+            -- Enable map for all MP gamemodes
             local cqlCategory = LevelDescriptionInclusionCategory()
             cqlCategory.category = 'GameMode'
             cqlCategory.mode:add('ConquestLarge0')
@@ -55,8 +83,7 @@ Events:Subscribe('Partition:Loaded', function(partition) -- Iterates through eve
             cqlCategory.mode:add('AirSuperiority0')
             thisInstance.categories:add(cqlCategory)
 
-            -- Remove SP 'StartPoints'
-            print('Removing StartPoints in \'' .. partition.name .. '\'...')
+            -- Remove SP/COOP 'StartPoints'
             thisInstance.startPoints:clear()
 
         end
@@ -77,7 +104,6 @@ Events:Subscribe('Partition:Loaded', function(partition) -- Iterates through eve
             thisInstance:MakeWritable()
 
             -- Allow all the parts of the level (SubWorlds) to be loaded simultaneously
-            print('Configuring autoload for \'' .. thisInstance.bundleName .. '\'...')
             thisInstance.autoLoad = true
 
             ::cont::
@@ -90,8 +116,13 @@ Events:Subscribe('Partition:Loaded', function(partition) -- Iterates through eve
             thisInstance:MakeWritable()
 
             -- Disable AI System
-            print('Disabling AI system...')
             thisInstance.aiSystem = nil
+
+            -- Set as MP in LevelData (redundant?)
+            local levelDataLvlDesc = LevelDescription(thisInstance.levelDescription)
+            levelDataLvlDesc.isCoop = false
+            levelDataLvlDesc.isMenu = false
+            levelDataLvlDesc.isMultiplayer = true
 
         end
 
@@ -101,7 +132,6 @@ Events:Subscribe('Partition:Loaded', function(partition) -- Iterates through eve
             thisInstance:MakeWritable()
 
             -- Disable fog which comes default on some levels, such as Comrades (SP_Paris) or Thunder Run (SP_Tank)
-            print('Disabling fog in \'' .. partition.name .. '\'...')
             thisInstance.enable = false
 
         end
@@ -130,5 +160,3 @@ ResourceManager:RegisterInstanceLoadHandler(Guid('3008B866-A95F-12E7-3E2A-C53B33
     LevelDescriptionInclusionCategory(thisInstance.categories[1]).mode:add('ConquestLarge0')
 
 end)
-
--- Put any InstanceLoadHandlers to exclude SubWorlds down here.
--- ext/Shared/SpLevelDataRemove.lua
+++ ext/Shared/SpLevelDataRemove.lua
@@ -17,16 +17,17 @@ Events:Subscribe('Partition:Loaded', function(partition) -- Iterates through eve
 
             --print('Intercepting WorldPart: ' .. thisInstance.blueprint.name)
 
-            -- If this is not an approved WorldPart from the list, do not continue
+            -- If this is an approved WorldPart from the list, skip past the code to exclude the WorldPart
             for i, v in pairs(approvedWorldPartRefGuids) do
                 if thisInstance.instanceGuid == Guid(approvedWorldPartRefGuids[i]) then
-                    --print('WorldPart approved.')
+                    print('WorldPart \'' .. tostring(thisInstance.blueprint.name) .. '\' approved.')
                     goto cont
                 end
             end
             
             thisInstance:MakeWritable()
 
+            -- Exclude
             --print('Excluding WorldPartReferenceObjectData \'' .. tostring(thisInstance.blueprint.name) .. '\' in partition \'' .. partition.name .. '\'...')
             thisInstance.excluded = true
 
@@ -38,7 +39,7 @@ Events:Subscribe('Partition:Loaded', function(partition) -- Iterates through eve
 
 end)
 
--- Clear connections (at some point will be necessary to remove logic associated with sound)
+-- Clear connections not related to sound
 --[[Events:Subscribe('Partition:Loaded', function(partition)
 
     -- Don't read any partition not referring to a SP or COOP map
@@ -52,10 +53,94 @@ end)
 
             local thisInstance = LevelData(instance)
             thisInstance:MakeWritable()
-            thisInstance.linkConnections:clear()
+            
+            -- Check all event connections. If any connection does not include a connection to/from sound data, delete it.
+            for i=#thisInstance.eventConnections, 1, -1 do
+
+                local thisCxn = EventConnection(thisInstance.eventConnections[i])
+
+                if thisCxn.source == nil or thisCxn.target == nil then break end
+
+                if thisCxn.source:Is('SoundAreaEntityData') == false 
+                and thisCxn.source:Is('SoundEntityData') == false
+                and thisCxn.source:Is('MixerEntityData') == false
+                and thisCxn.target:Is('SoundAreaEntityData') == false 
+                and thisCxn.target:Is('SoundEntityData') == false
+                and thisCxn.target:Is('MixerEntityData') == false then
+
+                    print('Found bad event connection: erasing entry '..(i)..' from type '..thisCxn.source.typeInfo.name..' to type '..thisCxn.target.typeInfo.name)
+                    thisInstance.eventConnections:erase(i)
+
+                end
+
+            end
+
+            -- Check all event connections. If any connection does not include a connection to/from sound data, delete it.
+            for i=#thisInstance.propertyConnections, 1, -1 do
+
+                local thisCxn = PropertyConnection(thisInstance.propertyConnections[i])
+
+                if thisCxn.source == nil or thisCxn.target == nil then break end
+
+                if thisCxn.source:Is('SoundAreaEntityData') == false 
+                and thisCxn.source:Is('SoundEntityData') == false
+                and thisCxn.source:Is('MixerEntityData') == false
+                and thisCxn.target:Is('SoundAreaEntityData') == false 
+                and thisCxn.target:Is('SoundEntityData') == false
+                and thisCxn.target:Is('MixerEntityData') == false then
+
+                    print('Found bad property connection: erasing entry '..(i)..' from type '..thisCxn.source.typeInfo.name..' to type '..thisCxn.target.typeInfo.name)
+                    thisInstance.propertyConnections:erase(i)
+
+                end
+
+            end
+
+            -- Check all link connections. If any connection does not include a connection to/from sound data, delete it.
+            for i=#thisInstance.linkConnections, 1, -1 do
+
+                local thisCxn = LinkConnection(thisInstance.linkConnections[i])
+
+                if thisCxn.source == nil or thisCxn.target == nil then break end
+
+                if thisCxn.source:Is('SoundAreaEntityData') == false 
+                and thisCxn.source:Is('SoundEntityData') == false
+                and thisCxn.source:Is('MixerEntityData') == false
+                and thisCxn.target:Is('SoundAreaEntityData') == false 
+                and thisCxn.target:Is('SoundEntityData') == false
+                and thisCxn.target:Is('MixerEntityData') == false then
+
+                    print('Found bad link connection: erasing entry '..(i)..' from type '..thisCxn.source.typeInfo.name..' to type '..thisCxn.target.typeInfo.name)
+                    thisInstance.linkConnections:erase(i)
+
+                end
+
+            end
 
         end
 
     end
+
+end)]]
+
+--[[ Just delete the whole ReferenceObject registry lol this is going to fucking crash
+
+Events:Subscribe('Partition:Loaded', function(partition) -- Iterates through every single partition so, if there is an instance in one of them we want to change, we can do so.
+
+    local levelName = SharedUtils:GetLevelName()
+    local gameModeName = SharedUtils:GetCurrentGameMode()
+
+    -- Don't read any partition that's nil or not referring to the main level partition of the currently loading map
+    if partition == nil or levelName == nil or partition.name ~= string.lower(levelName) then
+        return
+    end
+
+    local spLevelData = LevelData(partition.primaryInstance)
+
+    print('lol fuck off')
+
+    local spRegistry = RegistryContainer(spLevelData.registryContainer)
+    spRegistry:MakeWritable()
+    local spRefObjRegistry = spRegistry.referenceObjectRegistry:clear()
 
 end)]]
