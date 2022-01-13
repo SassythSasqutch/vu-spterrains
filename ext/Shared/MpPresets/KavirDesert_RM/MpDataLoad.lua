f1db8930e332d8f05d9f74b46d8fdc02639e0ef1
SassythSasqutch <sassythsasqutch@gmail.com>
2022-01-13 23:26:51 +0000

    fix(registries): removed registry loading

--- ext/Shared/MpPresets/KavirDesert_RM/MpDataLoad.lua
+++ ext/Shared/MpPresets/KavirDesert_RM/MpDataLoad.lua
@@ -25,21 +25,3 @@ Hooks:Install('ResourceManager:LoadBundles', 500, function(hook, bundles, compar
     end
 
 end)
-
--- Add resources to registry (not performed by RM)
-Events:Subscribe('Level:RegisterEntityResources', function(levelData)
-
-    local levelName = SharedUtils:GetLevelName()
-    local gameModeName = SharedUtils:GetCurrentGameMode()
-
-    if string.find(levelName, 'SP_Tank') == nil or gameModeName ~= 'ConquestLarge0' then
-        return
-    end
-
-    local firestormRegistry = ResourceManager:FindInstanceByGuid(Guid('8BB2506F-493A-11DF-B052-CC4B5D6E8131'), Guid('84238275-9EEE-D74E-7D3B-6FDD6A46594A'))
-    ResourceManager:AddRegistry(firestormRegistry, ResourceCompartment.ResourceCompartment_Game)
-
-    local firestormCqsRegistry = ResourceManager:FindInstanceByGuid(Guid('8DB9CB2A-2A16-44B8-927D-024F1AD06FCF'), Guid('320240BC-173A-5E32-CA75-51E15AC01313'))
-    ResourceManager:AddRegistry(firestormCqsRegistry, ResourceCompartment.ResourceCompartment_Game)
-
-end)
--- ext/Shared/MpPresets/PanjshirValley_RM/MpDataLoad.lua
+++ ext/Shared/MpPresets/PanjshirValley_RM/MpDataLoad.lua
@@ -26,22 +26,3 @@ Hooks:Install('ResourceManager:LoadBundles', 500, function(hook, bundles, compar
     end
 
 end)
-
--- Add resources to registry (not performed by RM)
-Events:Subscribe('Level:RegisterEntityResources', function(levelData)
-
-    local levelName = SharedUtils:GetLevelName()
-    local gameModeName = SharedUtils:GetCurrentGameMode()
-
-    -- Don't continue if the level is not Fire From The Sky Conquest Large
-    if string.find(levelName, 'COOP_006') == nil or gameModeName ~= 'ConquestLarge0' then
-        return
-    end
-
-    local damavandRegistry = ResourceManager:FindInstanceByGuid(Guid('8683F4BB-CD57-11DF-A251-CAB801F86363'), Guid('7C37A538-6C37-4BCF-4FF5-31E534B5C053'))
-    ResourceManager:AddRegistry(damavandRegistry, ResourceCompartment.ResourceCompartment_Game)
-
-    local damavandCqlRegistry = ResourceManager:FindInstanceByGuid(Guid('1CEC6C7E-1629-4631-B326-1A134BC6EF27'), Guid('6196137B-50D6-4607-98AE-900BACF47065'))
-    ResourceManager:AddRegistry(damavandCqlRegistry, ResourceCompartment.ResourceCompartment_Game)
-
-end)
