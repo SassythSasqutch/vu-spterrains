c0703f03ae0210a25fc9230f3b0f5b8ad92e0cd2
SassythSasqutch <37040523+SassythSasqutch@users.noreply.github.com>
2022-01-21 20:51:59 +0000

    fix(sp_tank): i am retarded

--- ext/Shared/MpPresets/KavirDesert_RM/CreateGameModeSubWorldRef.lua
+++ ext/Shared/MpPresets/KavirDesert_RM/CreateGameModeSubWorldRef.lua
@@ -13,15 +13,17 @@ Events:Subscribe('Partition:Loaded', function(partition)
 
     function getMpSubWorldRef()
         local firestormCqlSubWorldReferenceObjectData = SubWorldReferenceObjectData(ResourceManager:FindInstanceByGuid(Guid('8BB2506F-493A-11DF-B052-CC4B5D6E8131'), Guid('8FE1F5F4-6C8F-4185-B478-2DDEA1CCA686')))
+        return firestormCqlSubWorldReferenceObjectData
     end
 
     if not pcall(getMpSubWorldRef) then
         error('WARNING: Multiplayer logic could not be found. This is catastrophic. Ensure that you haven\'t modified bundle orders.')
+        return
     end
 
     -- Add to LevelData 'Objects' array
     local spLevelData = LevelData(partition.primaryInstance)
     spLevelData:MakeWritable()
-    spLevelData.objects:add(firestormCqlSubWorldReferenceObjectData)
+    spLevelData.objects:add(getMpSubWorldRef())
 
 end)
