e1237bbd8f1821fb65c73c0d2cba1f2e418497af
SassythSasqutch <37040523+SassythSasqutch@users.noreply.github.com>
2022-01-21 20:23:10 +0000

    fix(sp_tank): account for changed bundle order and add an error message if the MP SubWorldRef cannot be found

--- ext/Shared/MpPresets/KavirDesert_RM/CreateGameModeSubWorldRef.lua
+++ ext/Shared/MpPresets/KavirDesert_RM/CreateGameModeSubWorldRef.lua
@@ -11,15 +11,17 @@ Events:Subscribe('Partition:Loaded', function(partition)
         return
     end
 
-    ResourceManager:RegisterInstanceLoadHandler(Guid('8BB2506F-493A-11DF-B052-CC4B5D6E8131'), Guid('8FE1F5F4-6C8F-4185-B478-2DDEA1CCA686'), function(instance)
+    function getMpSubWorldRef()
+        local firestormCqlSubWorldReferenceObjectData = SubWorldReferenceObjectData(ResourceManager:FindInstanceByGuid(Guid('8BB2506F-493A-11DF-B052-CC4B5D6E8131'), Guid('8FE1F5F4-6C8F-4185-B478-2DDEA1CCA686')))
+    end
 
-        local firestormCqlSubWorldReferenceObjectData = SubWorldReferenceObjectData(instance)
+    if not pcall(getMpSubWorldRef) then
+        error('WARNING: Multiplayer logic could not be found. This is catastrophic. Ensure that you haven\'t modified bundle orders.')
+    end
 
-        -- Add to LevelData 'Objects' array
-        local spLevelData = LevelData(partition.primaryInstance)
-        spLevelData:MakeWritable()
-        spLevelData.objects:add(firestormCqlSubWorldReferenceObjectData)
-
-    end)
+    -- Add to LevelData 'Objects' array
+    local spLevelData = LevelData(partition.primaryInstance)
+    spLevelData:MakeWritable()
+    spLevelData.objects:add(firestormCqlSubWorldReferenceObjectData)
 
 end)
